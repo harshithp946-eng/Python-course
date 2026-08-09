1fa6506827a3eba5c1e566c1d2224d86bb72a0f1
harshithp946-eng <harshithp946@gmail.com>
2026-08-09 11:18:55 +0530

    prints the random joke

---
+++ module.py
@@ -0,0 +1,5 @@
+import pyjokes
+# this prints the random joke from the pyjokes library.
+# to install pyjokes, in the terminal run the command: pip install pyjokes
+joke = pyjokes.get_joke()
+print(joke)

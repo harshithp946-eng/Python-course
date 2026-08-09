60e981167da02291820407ec4ef1f1b42c04198c
harshithp946-eng <harshithp946@gmail.com>
2026-08-09 20:36:25 +0530

    Operators

---
+++ chapter2/Operators.py
@@ -0,0 +1,36 @@
+# Arithmetic Operators
+a=5
+b=4
+c=a+b # addition
+print(c)
+
+
+#Assignment Operators
+a=5-3 #assigns the value of 5-3 to a
+print(a)
+b=5
+# b+=3 # adds 3 to b and assigns the value to b
+b-=3 # subtracts 3 from b and assigns the value to b
+print(b)
+
+
+#Comparison Operators
+d=5!=4
+print(d)
+
+#logical Operators
+e=True or False
+# Truth table for or(+) operator
+print("True or False:",True or False )
+print("True or True:",True or True )
+print("False or False:",False or False )
+print("False or True:",False or True )
+
+# Truth table for and(*) operator
+print("True and False:",True and False )
+print("True and True:",True and True )
+print("False and False:",False and False )
+print("False and True:",False and True )
+
+
+print(not True )

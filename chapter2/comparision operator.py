680b6980d43b357ae5c7cfacd041ba5360c2048e
harshithp946-eng <harshithp946@gmail.com>
2026-08-12 20:43:13 +0530

    Comparision operators

---
+++ chapter2/comparision operator.py
@@ -0,0 +1,8 @@
+a =int(input("Enter the value of a: "))
+b =int(input("Enter the value of b: "))
+print("Comparison of a and b",a>b)
+print("Comparison of a and b",a<b)
+print("Comparison of a and b",a==b)
+print("Comparison of a and b",a!=b)
+print("Comparison of a and b",a>=b)
+print("Comparison of a and b",a<=b)

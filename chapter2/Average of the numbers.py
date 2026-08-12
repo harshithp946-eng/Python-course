422b73570e68ab61629a8a0e8047ba07a3c6950a
harshithp946-eng <harshithp946@gmail.com>
2026-08-12 20:52:53 +0530

    average of three numbers

---
+++ chapter2/Average of the numbers.py
@@ -0,0 +1,5 @@
+a =int(input("Enter the value of a: "))
+b =int(input("Enter the value of b: "))
+c =int(input("Enter the value of c: "))
+average = (a + b + c) / 3;
+print("The average is: ", average)
